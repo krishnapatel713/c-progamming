c2ee8638fa7ff490d8f1802559b4142e6806205f
krishnapatel713 <krishnavpatel2006@gmail.com>
2025-08-19 01:53:14 +0530

    Update celcius into fahrenheit.c

--- celcius into fahrenheit.c
+++ celcius into fahrenheit.c
@@ -4,6 +4,6 @@ int main()
      float c,f;
      printf("Temperature in Celcius=");
      scanf("%f",&c);
-     f=(9/5*c)+32;
+     f=(9.0/5.0*c)+32;
      printf("Temperature in Fahrenheit=%f",f);
 }
