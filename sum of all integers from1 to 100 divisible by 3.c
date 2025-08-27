854d4cfec85b785c5c398de5f52a2aa8640d34a5
krishnapatel713 <krishnavpatel2006@gmail.com>
2025-08-27 17:04:22 +0530

    Update and rename sum of all integers from1 to 100 divisibleby5.c to sum of all integers from1 to 100 divisible by 3.c

--- sum of all integers from1 to 100 divisible by 3.c
+++ sum of all integers from1 to 100 divisible by 3.c
@@ -4,11 +4,11 @@ int main()
     int i, sum = 0;
     for(i = 1; i <= 100; i++)
     {
-        if(i % 5 == 0)
+        if(i % 3 == 0)
         {
             sum = sum + i;
         }
     }
-    printf("Sum of integers divisible by 5 from 1 to 100 = %d\n", sum);
+    printf("Sum of integers divisible by 3 from 1 to 100 = %d\n", sum);
     return 0;
 }
